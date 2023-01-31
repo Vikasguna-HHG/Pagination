b80f4557bd7d299b4633ced44ac1f9dc74fcc006
vikas <101402071+Vikasguna-HHG@users.noreply.github.com>
2023-01-31 18:49:25 +0530

    Update Home.js

--- Home.js
+++ Home.js
@@ -1,166 +1,88 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Card, Button, Container } from 'react-bootstrap';
-import Pagination from './Pagination';
-import { Link, NavLink } from 'react-router-dom';
-function Home() {
+import React, { useState, useRef } from 'react'
+import axios from 'axios';
 
-    const [list, setList] = useState([]);
-    const [category, setcategory] = useState(list);
-    const [company, setcompany] = useState(list);
-    const [colors, setcolors] = useState(list);
-    const [currentPage, setCurrentPage] = useState(1);
-    const [postsPerPage, setPostsPerPage] = useState(10);
+function Search1() {
 
+    const [searchValue, setSearchValue] = useState("");
+    const [searchResult, setSearchResult] = useState("");
+    const [show, setShow] = useState(false);
+    const [typingTimeout, setTypingTimeout] = useState("");
 
-    useEffect(() => {
-        axios.get('https://api.pujakaitem.com/api/products').then((res) => {
-            setList(res.data)
-        })
-    }, [])
-    const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = list.slice(firstPostIndex, lastPostIndex);
-
-    const handlecategory = (e) => {
-
-        if (e.target.checked === false) {
-            setcategory("")
-        } else if (e.target.value === "All") {
-            categoryData.map((item, i) => {
-                return setcategory(item);
-            })
-            // setcategory(categoryData)
-        } else {
-            setcategory(e.target.value);
+    function handleSearch(e) {
+        debugger
+        if (e.target.value == "") {
+            setSearchResult([]);
+            setSearchValue("");
+            return false;
         }
 
+        setSearchValue(e.target.value);
 
+        clearTimeout(typingTimeout);
+        setTypingTimeout(setTimeout(() => searchData(e.target.value), 500));
     }
 
-    console.log(category);
-
-    const handlecompany = (e) => {
-        if (e.target.checked === false) {
-            setcompany("")
-        } else if (e.target.value === "All") {
-            companyData.map((item, i) => {
-                return setcompany(item);
-            })
-            // setcompany(categoryData)
-        } else {
-            setcompany(e.target.value);
-        }
+    function handle(value) {
+        setShow(value);
+        document.addEventListener("click", handleClickOutside, true);
     }
 
-
-    const handlecolor = (e) => {
-        if (e.target.checked === false) {
-            setcolors("")
-        } else if (e.target.value === "All") {
-            companyData.map((item, i) => {
-                return setcolors(item);
-            })
-            // setcolors(categoryData)
-        } else {
-            setcolors(e.target.value);
-        }
-    }
-
-
-    const getUniqueData = (data, property) => {
-        let newVal = data.map((item, i) => {
-            return item[property];
+    function searchData(s) {
+        axios.post("http://localhost:5000/get?name=" + s).then((res) => {
+            setSearchResult(res.data.data);
         });
+    }
 
-        if (property === "colors") {
-            // return (newVal = ["All", ...new Set([].concat(...newVal))]);
-            newVal = newVal.flat();
+    const ref = useRef(null);
+    const handleClickOutside = (event) => {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setShow(false);
+            setSearchResult([]);
         }
-
-        // return (newVal = ["All", ...new Set(newVal)]);
-        return (newVal = [...new Set(newVal)]);
-
     };
 
-    const categoryData = getUniqueData(list, "category");
-    const companyData = getUniqueData(list, "company");
-    const colorsData = getUniqueData(list, "colors");
-    const price = getUniqueData(list, "price");
 
+    function Search() {
+        if (searchResult.length == 0)
+            return <div className="searchdrop" id="search-result"></div>;
 
-    const max = Math.max(...price)
+        return (
+            <div className="searchdrop" id="search-result" ref={ref}>
+                {searchResult.map((suggesstion, index) => {
+                    if (index < 5) {
+                        return (
+                            <div key={index} className="searcdropbg">
+                                <h5>{suggesstion.name}</h5>
+                            </div>
+                        );
+                    } else {
+                        return null;
+                    }
+                })}
+            </div>
+        );
+    }
+
+    console.log(searchResult);
     return (
         <div>
-            {categoryData.map((item, i) => {
-                return (
-                    <>
-                        <input key={i} type='checkbox' name='category' value={item} onClick={handlecategory} />{item}
-                    </>
-                )
-            })}
-
-            <br />
-            {companyData.map((item, i) => {
-                return (
-                    <>
-                        <input key={i} type='checkbox' name='company' value={item} onClick={handlecompany} />{item}
-                    </>
-                )
-            })}
-
-            <br />
-            {colorsData.map((item, i) => {
-                return (
-                    <>
-                        {/* <button key={i} type='button' name='colors' value={item} onClick={handlecategory} style={{ color: { item } }} >{item}</button> */}
-                        <input key={i} type='checkbox' name='colors' value={item} onClick={handlecolor} />{item}
-                    </>
-                )
-            })}
-
-            {
-                currentPosts.filter((data, i) => { return data.category === category || data.company === company || data.colors[i++] === colors }).map((item, i) => {
-                    return (
-                        <>
-                            <Container>
-                                <div className='row'>
-                                    <div className='col'>
-                                        <Card style={{ width: '18rem' }} key={i} >
-                                            <NavLink to={`Link/${item.id}`}>
-                                                <Card.Img variant="top" src={item.image} />
-                                            </NavLink>
-                                            <Card.Body>
-                                                <Card.Title>{item.name}</Card.Title>
-                                                <Card.Text>
-                                                    {item.fully_diluted_valuation}
-                                                </Card.Text>
-                                                <p>{item.company}</p>
-                                                <p>{item.price}</p>
-                                                <p>{item.category}</p>
-
-                                                <Button variant="primary">Go somewhere</Button>
-                                            </Card.Body>
-                                        </Card>
-
-                                    </div>
-                                </div>
-                            </Container>
-                        </>
-                    )
-                })
-            }
-
-
-            <Pagination
-                totalPosts={list.length}
-                postsPerPage={postsPerPage}
-                setCurrentPage={setCurrentPage}
-                currentPage={currentPage}
-            />
+            <div className="col-md-3 col-sm-4 col-xs-12">
+                <i className="fa fa-search myicon" aria-hidden="true"></i>
+                <input
+                    type="text"
+                    className="search-click"
+                    name=""
+                    onChange={handleSearch}
+                    onClick={() => {
+                        handle(true);
+                    }}
+                    placeholder="Search Here..."
+                    id="search-text"
+                />
+                <Search />
+            </div>
         </div>
     )
 }
 
-export default Home
+export default Search1
